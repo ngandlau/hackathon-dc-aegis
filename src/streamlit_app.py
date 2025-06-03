f449fd7a1b0a0487705f14dc2f1124e4e864703d
ngandlau <28842354+ngandlau@users.noreply.github.com>
2025-06-03 16:30:37 -0400

    fixes imports

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,14 +1,9 @@
-import os
-import sys
 import time
 
 import pandas as pd
 import streamlit as st
 
-# Add the current directory to Python path so we can import utils
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-from utils import (
+from src.utils import (
     fetch_disease_data,
     generate_recommendations,
     save_plot,
